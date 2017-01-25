bac2862d8455e28630a10b53d9bf85ca6df7c674
Joe <josephsanglee@gmail.com>
2017-01-25 01:04:03 -0800

    complete palindromeChecker

---
+++ palindromeChecker.js
@@ -0,0 +1,49 @@
+/*******
+** 
+** Write an efficient function that checks whether any permutation of an input string is a palindrome.
+** 
+** Examples:
+** "civic" should return true
+** "ivicc" should return true
+** "civil" should return false
+** "livci" should return false
+**
+*******/
+
+const palindromeChecker = (string) => {
+  string = string.replace(/ /g, '').toLowerCase();
+
+  // create a hash table of the counts of all of the characters
+  // currently found. At most, the size of the chars will be 
+  // O(26), i.e. the size of the alphabet.
+  const chars = {};
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+
+    chars[char] = chars[char] ? chars[char] + 1 : 1;
+  }
+
+  // in order for a string to be a palindrome, only ONE
+  // char can have an odd character count.
+  let oddCharCount = false;
+
+  for (let char in chars) {
+    if (chars[char] % 2 !== 0) {
+      if (!oddCharCount) {
+        oddCharCount = true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+console.log(palindromeChecker('civic')); //should return true
+console.log(palindromeChecker('ivicc')); //should return true
+console.log(palindromeChecker('civil')); //should return false
+console.log(palindromeChecker('livci')); //should return false
+console.log(palindromeChecker('racecar')); //should return true
+console.log(palindromeChecker('acecarr')); //should return true
