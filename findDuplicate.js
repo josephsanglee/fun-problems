c9666db1e0c477e9a1e851368420f371797aef77
Joe <josephsanglee@gmail.com>
2017-01-27 00:53:21 -0800

    Complete findDuplicate

---
+++ findDuplicate.js
@@ -0,0 +1,15 @@
+const findDuplicate = (array, n) => {
+  let totalSum = 0;
+  
+  for (var i = 1; i <= n; i++) {
+    totalSum += i;
+  }
+  console.log(totalSum);
+  let arraySum = array.reduce((a, b) => a + b);
+
+  return arraySum - totalSum;
+};
+
+
+const nums = [1, 2, 3, 3, 4, 5];
+console.log(findDuplicate(nums, 5));
