fa422461c6b64262577e309ae50ba5c3402073a7
Joe <josephsanglee@gmail.com>
2017-01-26 23:32:41 -0800

    Implement cleaner solution

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -10,35 +10,51 @@
 **
 *******/
 
-const palindromeChecker = (string) => {
-  string = string.replace(/ /g, '').toLowerCase();
+// const palindromeChecker = (string) => {
+//   string = string.replace(/ /g, '').toLowerCase();
 
-  // create a hash table of the counts of all of the characters
-  // currently found. At most, the size of the chars will be 
-  // O(26), i.e. the size of the alphabet.
-  const chars = {};
+//   // create a hash table of the counts of all of the characters
+//   // currently found. At most, the size of the chars will be 
+//   // O(26), i.e. the size of the alphabet.
+//   const chars = {};
+
+//   for (let i = 0; i < string.length; i++) {
+//     let char = string[i];
+
+//     chars[char] = chars[char] ? chars[char] + 1 : 1;
+//   }
+
+//   // in order for a string to be a palindrome, only ONE
+//   // char can have an odd character count.
+//   let oddCharCount = false;
+
+//   for (let char in chars) {
+//     if (chars[char] % 2 !== 0) {
+//       if (!oddCharCount) {
+//         oddCharCount = true;
+//       } else {
+//         return false;
+//       }
+//     }
+//   }
+
+//   return true;
+// };
+
+const palindromeChecker = (string) => {
+  const unpairedChars = new Set();
 
   for (let i = 0; i < string.length; i++) {
     let char = string[i];
 
-    chars[char] = chars[char] ? chars[char] + 1 : 1;
-  }
-
-  // in order for a string to be a palindrome, only ONE
-  // char can have an odd character count.
-  let oddCharCount = false;
-
-  for (let char in chars) {
-    if (chars[char] % 2 !== 0) {
-      if (!oddCharCount) {
-        oddCharCount = true;
-      } else {
-        return false;
-      }
+    if (unpairedChars.has(char)) {
+      unpairedChars.delete(char);
+    } else {
+      unpairedChars.add(char);
     }
   }
 
-  return true;
+  return unpairedChars.size <= 1;
 };
 
 console.log(palindromeChecker('civic')); //should return true
