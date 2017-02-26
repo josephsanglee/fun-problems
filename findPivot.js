8d32c8d7f285eb0ae4bd70f55842076d5f22a1f5
Joe <josephsanglee@gmail.com>
2017-02-25 18:15:11 -0800

    Modify findPivot

--- findPivot.js
+++ findPivot.js
@@ -33,21 +33,21 @@ const findPivot = (ints) => {
   // entire array is sorted and there is no pivot.
   // the loop will increase the start index to be greater
   // than the end index
-  while (start <= end) {
+  while (start < end) {
     let mid = Math.floor((start + end) / 2);
 
-    // if true, ints[mid + 1] is the pivot
-    if (ints[mid + 1] < ints[mid]) { return mid + 1; }
+    // if true, ints[mid] is the pivot
+    if (ints[mid] < ints[mid - 1]) { return mid; }
 
     // if the int at the start index is less than the int
     // than the int at the mid index, that means all of the values
     // before the mid index are sorted. Therefore, the pivot MUST 
     // be in the second half of the array. Else, the pivot MUST be
     // in the first half of the array.
-    ints[start] <= ints[mid] ? start = mid + 1 : end = mid - 1;
+    ints[start] < ints[mid] ? start = mid : end = mid;
   }
 
-  return -1;
+  return 0;
 };
 //O(logN) time complexity
 //O(1) space complexity
@@ -55,5 +55,5 @@ const findPivot = (ints) => {
 var ints1 = [3, 4, 5, 6, 1, 2];
 console.log(findPivot(ints1)); // 4
 
-var ints2 = [1, 2, 3, 4, 5, 6];
-console.log(findPivot(ints2)); // -1
+var ints2 = [7, 1, 2, 3, 4, 5, 6];
+console.log(findPivot(ints2)); // 1
